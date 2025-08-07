83944de256ab902bf78642c6bd91512f2e39988e
adart <icetea7578@gmail.com>
2025-08-07 21:40:41 +0700

    [startdwm] suppressed dms and related stuff

--- .opt/startdwm.sh
+++ .opt/startdwm.sh
@@ -1,7 +1,11 @@
 xrandr --output HDMI-1 --mode 2560x1440
+
 feh --bg-scale /usr/share/wallpapers/wallhaven-1ppgmv.jpg
-slstatus &
-xautolock -time 10 -locker slock &
+xset s off -dpms s noblank
+#xautolock -time 10 -locker slock &
+
 picom -b
+slstatus &
 /usr/bin/dunst &
+
 exec /sbin/bin/dwm
